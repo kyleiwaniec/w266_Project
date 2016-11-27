dd337ced1c8dc4909553a3e9718dcc737e2a1817
Tom W. Yedwab <tom@khanacademy.org>
2016-11-27 04:02:11 -0800

    Updated preprocessing and tf-idf features.

--- models/features/common.py
+++ models/features/common.py
@@ -5,30 +5,36 @@ import numpy as np
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import LinearSVC
+import scipy.sparse
 
-dataset = None
+datasets = {}
 
-def load_dataset():
-    global dataset
-    if not dataset:
-        print "Loading ka-comments-balanced dataset."
-        with open("/usr/src/app/data/ka-comments-balanced.pickle", "rb") as f:
-            dataset = pickle.load(f)        
-    return dataset
+def load_dataset(dataset_name):
+    global datasets
+    if dataset_name not in datasets:
+        print "Loading %s dataset." % dataset_name
+        with open("/usr/src/app/data/%s.pickle" % dataset_name, "rb") as f:
+            datasets[dataset_name] = pickle.load(f)
+    return datasets[dataset_name]
 
-def extract_features(features_def, features_name):
-    dataset = load_dataset()
+def extract_features(dataset_name, features_def, features_name):
+    dataset = load_dataset(dataset_name)
     
     print "Training feature extractor %s." % features_name
     
     features = {}
     features["train_X"] = features_def.train(dataset["train_data"])
     features["train_Y"] = dataset["train_data"]["hasVotes"]
+    
+    print "Generating validation set..."
     features["validate_X"] = features_def.transform(dataset["validate_data"])
     features["validate_Y"] = dataset["validate_data"]["hasVotes"]
+    
+    print "Generating test set..."
     features["test_X"] = features_def.transform(dataset["test_data"])
     features["test_Y"] = dataset["test_data"]["hasVotes"]
     
+    print "Writing to disk..."
     with open("/usr/src/app/model-data/%s.pickle" % features_name, "wb") as f:
         pickle.dump(features, f)
         
@@ -51,3 +57,27 @@ def test_features(features_name):
         model.fit(features['train_X'], features['train_Y'])
         score = model.score(features['validate_X'], features['validate_Y'])
         print "## %20s %15s accuracy: %0.1f %%" % (model_name, features_name, score * 100)
+        
+def test_combined_features(feature_names):
+    in_features = []
+    for features_name in feature_names:
+        print "Loading features %s." % features_name
+        with open("/usr/src/app/model-data/%s.pickle" % features_name, "rb") as f:
+            in_features.append(pickle.load(f))
+            
+    print "Combining features."
+
+    features = {
+        "train_X": scipy.sparse.hstack([f["train_X"] for f in in_features]),
+        "train_Y": in_features[0]["train_Y"],
+        "validate_X": scipy.sparse.hstack([f["validate_X"] for f in in_features]),
+        "validate_Y": in_features[0]["validate_Y"],
+    }
+    
+    combined_name = "_".join(feature_names)
+        
+    print "Training models."
+    for model_name, model in model_types():
+        model.fit(features['train_X'], features['train_Y'])
+        score = model.score(features['validate_X'], features['validate_Y'])
+        print "## %20s %30s accuracy: %0.1f %%" % (model_name, combined_name, score * 100)
